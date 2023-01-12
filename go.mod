97140c601cab351792f4f27fa2060cd08ad1554d
Ogtay Garayev <okaraev@outlook.com>
2023-01-12 14:13:34 +0400

    first init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module WebApp
+
+go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.8.2
+	github.com/prometheus/client_golang v1.14.0
+)
---
+++ web.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+type httpresponse struct {
+	Status  bool
+	Message string
+}
+
+var buckets = []float64{.001, .002, .003, .005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5, 10}
+var (
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "webapp_processed_requests_total",
+		Help: "The total number of requests",
+	})
+)
+
+var (
+	hist = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: "webapp",
+			Name:      "http_server_request_duration_seconds",
+			Help:      "Histogram of response time for handler in seconds",
+			Buckets:   buckets,
+		},
+		[]string{"route", "method", "status_code"},
+	)
+)
+
+func GetSample(c *gin.Context) {
+	rand := rand.Intn(400)
+	sleepDuration := time.Duration(rand * int(time.Millisecond))
+	time.Sleep(sleepDuration)
+	opsProcessed.Inc()
+	obs := float64(rand) / 1000
+	fmt.Println("Observing:", obs)
+	hist.WithLabelValues("/get", "GET", "200").Observe(obs)
+	c.IndentedJSON(200, httpresponse{Status: true, Message: "Success"})
+}
+
+func main() {
+
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		http.ListenAndServe(":9101", nil)
+	}()
+	prometheus.MustRegister(hist)
+	port := os.Getenv("HTTP_PORT")
+	if port == "" {
+		port = "8086"
+	}
+	router := gin.Default()
+	router.GET("/get", GetSample)
+	router.Run(":" + port)
+}
