88cf44f9689e2d7f6abbbf6f329e54a953e7b570
Ogtay Garayev <okaraev@outlook.com>
2023-01-13 19:17:08 +0400

    bug

--- web.go
+++ web.go
@@ -38,7 +38,7 @@ var (
 )
 
 func GetSample(c *gin.Context) {
-	rand := rand.Intn(400)
+	rand := rand.Intn(600)
 	sleepDuration := time.Duration(rand * int(time.Millisecond))
 	time.Sleep(sleepDuration)
 	opsProcessed.Inc()
