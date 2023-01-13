e9d845275f8fa574d47034e9a3f5173bac76d0c1
Ogtay Garayev <okaraev@outlook.com>
2023-01-13 19:56:47 +0400

    bug fixv3

--- web.go
+++ web.go
@@ -17,7 +17,7 @@ type httpresponse struct {
 	Message string
 }
 
-var buckets = []float64{.1, .25, .5, .9, .99, .999, 1, 2.5}
+var buckets = []float64{.05, .1, .25, .5, .9, .99, .999, 1, 2.5}
 var (
 	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "webapp_processed_requests_total",
