f2531a86a0fd5c5de2916268ac1f689a73e86213
Ogtay Garayev <okaraev@outlook.com>
2023-01-15 12:41:35 +0400

    performance improvement

--- web.go
+++ web.go
@@ -38,7 +38,7 @@ var (
 )
 
 func GetSample(c *gin.Context) {
-	rand := rand.Intn(50)
+	rand := rand.Intn(10)
 	sleepDuration := time.Duration(rand * int(time.Millisecond))
 	time.Sleep(sleepDuration)
 	opsProcessed.Inc()
